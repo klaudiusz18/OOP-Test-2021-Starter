25656072738f1709019417a874034073793a6d54
klaudiusz <klaudiusz443@gmail.com>
2021-04-21 19:54:22 +0100

    2nd commit

--- java/src/ie/tudublin/Note.java
+++ java/src/ie/tudublin/Note.java
@@ -8,3 +8,40 @@ public class Note {
 
     
 
+// to string method
+    @Override
+    public String toString() {
+        return "Note [duration=" + duration + ", note=" + note + "]";
+    }
+
+    public Note()
+    {
+
+    }
+
+
+    //constructors
+    public Note(char note, int duration)
+    {
+        this.note = note;
+        this.duration = duration;
+    }
+
+
+
+
+// getters and setters
+    public char getNote() {
+        return note;
+    }
+    public void setNote(char note) {
+        this.note = note;
+    }
+    public int getDuration() {
+        return duration;
+    }
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+    
+}
